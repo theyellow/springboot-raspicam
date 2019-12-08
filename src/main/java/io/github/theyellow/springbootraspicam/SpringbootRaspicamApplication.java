4b061702dabf237b69a2ac79aad05bc9e07bd40e
Benjamin Marstaller <benjamin@marstaller.org>
2019-12-08 13:37:09 +0100

    initialize repository

---
+++ src/main/java/io/github/theyellow/springbootraspicam/SpringbootRaspicamApplication.java
@@ -0,0 +1,13 @@
+package io.github.theyellow.springbootraspicam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRaspicamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRaspicamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/theyellow/springbootraspicam/SpringbootRaspicamApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.theyellow.springbootraspicam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRaspicamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
