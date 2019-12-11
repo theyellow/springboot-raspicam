4eaf581561f43b8e983cc4fa88886dc1dffe9903
Benjamin Marstaller <benjamin@marstaller.org>
2019-12-11 14:35:50 +0100

    add recordVideo methods to camera controller

--- springboot-raspicam-jar/src/main/java/io/github/theyellow/springbootraspicam/CameraController.java
+++ springboot-raspicam-jar/src/main/java/io/github/theyellow/springbootraspicam/CameraController.java
@@ -21,11 +21,19 @@ import uk.co.caprica.picam.enums.Encoding;
 @Controller
 public class CameraController {
 
+	private static final int DEFAULT_VIDEO_HEIGHT = 480;
+
+	private static final int DEFAULT_VIDEO_WIDTH = 640;
+
+	private static final String DEFAULT_VIDEO_NAME = "video.mp4";
+
+	private static final int DEFAULT_LENGTH_IN_MILLIS = 20000;
+
 	private final Logger logger = LoggerFactory.getLogger(CameraController.class);
 
 	private final CameraConfiguration configuration;
 
-	private final Semaphore cameraUse = new Semaphore(1);
+	private static final Semaphore cameraUse = new Semaphore(1);
 
 	public CameraController() {
 		Environment.dumpEnvironment();
@@ -75,17 +83,83 @@ public class CameraController {
 // 		@formatter:on
 	}
 
-	public File recordVideo() throws FailedToRunRaspistillException, IOException, InterruptedException {
+	public File recordVideo(String name, Integer width, Integer height, Integer lengthInMillis)
+			throws FailedToRunRaspistillException, IOException, InterruptedException {
+
 		boolean acquired = cameraUse.tryAcquire(10, TimeUnit.SECONDS);
+
 		if (acquired) {
 			RPiCamera rPiCamera = new RPiCamera("~");
-			File recordVideo = rPiCamera.recordVideo("video.mp4", 640, 430, 20000, true);
+			File recordVideo = rPiCamera.recordVideo(name, width, height, lengthInMillis, true);
 			cameraUse.release();
+			logger.info("recorded video {} with width {} and height {} and a length of {} successfully",
+					recordVideo.getAbsolutePath(), width, height, lengthInMillis);
+
 			return recordVideo;
 		} else {
+			logger.warn("Could not get camera within the last 10 seconds exclusively, so quit and return null");
 			return null;
 		}
 	}
+
+	/**
+	 * Convenience-method for test-video with default name "video.mp4" placed under
+	 * ~/ <br/>
+	 * <br/>
+	 * HEIGHT,WIDTH and LENGTH (in millis) are also standard values (640, 480,
+	 * 20000).
+	 *
+	 * @return file-handle with test-video
+	 * 
+	 * @throws FailedToRunRaspistillException
+	 *             raspivid-exception
+	 * @throws IOException
+	 *             problems with IO
+	 * @throws InterruptedException
+	 *             problems with synchronising camera resource internally
+	 */
+	public File recordVideo() throws FailedToRunRaspistillException, IOException, InterruptedException {
+		return recordVideo(DEFAULT_VIDEO_NAME, DEFAULT_VIDEO_WIDTH, DEFAULT_VIDEO_HEIGHT, DEFAULT_LENGTH_IN_MILLIS);
+	}
+
+	/**
+	 * 
+	 * Record video with a given name and length in millis. Resolution will be
+	 * default (640, 480) normally
+	 * 
+	 * @param name
+	 *            name of video
+	 * @param length
+	 *            length of video (in millis)
+	 * 
+	 * @return file-handle with test-video
+	 * 
+	 * @throws FailedToRunRaspistillException
+	 *             raspivid-exception
+	 * @throws IOException
+	 *             problems with IO
+	 * @throws InterruptedException
+	 *             problems with synchronising camera resource internally
+	 */
+	public File recordVideo(String name, Integer length)
+			throws FailedToRunRaspistillException, IOException, InterruptedException {
+		return recordVideo(name, DEFAULT_VIDEO_WIDTH, DEFAULT_VIDEO_HEIGHT, length);
+	}
+
+	public File recordVideo(Integer length) throws FailedToRunRaspistillException, IOException, InterruptedException {
+		return recordVideo(DEFAULT_VIDEO_NAME, DEFAULT_VIDEO_WIDTH, DEFAULT_VIDEO_HEIGHT, length);
+	}
+
+	public File recordVideo(String name, Integer width, Integer height)
+			throws FailedToRunRaspistillException, IOException, InterruptedException {
+		return recordVideo(name, width, height, DEFAULT_LENGTH_IN_MILLIS);
+	}
+
+	// public File recordVideo(String name, Integer width, Integer height, Integer
+	// lengthInMillis) throws FailedToRunRaspistillException, IOException,
+	// InterruptedException {
+	// return recordVideo("video.mp4");
+	// }
 
 	public File takePicture() {
 
